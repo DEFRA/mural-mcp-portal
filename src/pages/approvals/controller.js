80ad0b3b948c4d07c198987f5fe960472fea9127
sfitz42 <87677173+sfitz42@users.noreply.github.com>
2026-09-01 11:14:17 +0100

    Fix sonar issues

--- src/pages/approvals/controller.js
+++ src/pages/approvals/controller.js
@@ -11,12 +11,12 @@ import { statusCodes } from '../../constants/status-codes.js'
  * content here yet, so forcing the OAuth connection flow would only add
  * friction for a page that has nothing to show either way.
  *
- * @param {import('@hapi/hapi').Request} request - Hapi request object
+ * @param {import('@hapi/hapi').Request} _request - Hapi request object
  * @param {import('@hapi/hapi').ResponseToolkit} h - Hapi response toolkit
  *
  * @returns {import('@hapi/hapi').ResponseObject} The response object for the approvals page
  */
-async function getApprovals (request, h) {
+async function getApprovals (_request, h) {
   return h.view('approvals/page.njk', { pageTitle: 'My approvals' })
     .code(statusCodes.HTTP_STATUS_OK)
 }
